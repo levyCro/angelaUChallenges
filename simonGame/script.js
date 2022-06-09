96ff376003f330763491b35223f5f75d0c49ba7f
Levy Crols <103933738+levyCro@users.noreply.github.com>
2022-06-09 14:23:45 +0200

    adding function for animation adding a style and removing with setTimeout

--- simonGame/script.js
+++ simonGame/script.js
@@ -3,28 +3,44 @@ let gamePattern = []
 let userClickedPattern = []
 
 // detect button click and store it in userClickedPattern
-$('.btn').on('click', function(){
-    let juserChosenColour = $(this).attr('id')
+$('.btn').on('click', function () {
+    let userChosenColour = $(this).attr('id')
 
-    userClickedPattern.push(juserChosenColour)
-    console.log(userClickedPattern)
-    
+    userClickedPattern.push(userChosenColour)
+
+    playSound(userChosenColour)
+
+    animatePress(userChosenColour)
+
 })
+
 
 // fuction to get random number and play sounds
 function nextSequence() {
     let randomNumber = buttonColours[Math.floor(Math.random() * buttonColours.length)];
-    
-    gamePattern.push(randomNumber)
-    $("#"+ randomNumber).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100)
-    
 
-    var audio = new Audio("sounds/"+ randomNumber +".mp3");
+    gamePattern.push(randomNumber)
+    $("#" + randomNumber).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100)
+
+    playSound(randomNumber)
+
+    animatePress(randomNumber)
+
+}
+//create function for sounds
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
-    
-    
+}
+
+// function for animation css
+function animatePress(currentColor) {
+
+    $('#'+ currentColor).addClass('pressed')
+
+    setTimeout(function () {
+        $('#'+ currentColor).removeClass('pressed')
+    }, 100)
 }
 
 nextSequence()
-
-
